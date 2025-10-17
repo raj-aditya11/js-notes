b97c7e9cb1dd4e866483c9dc5e069d74f503a63d
Aditya Raj <raj.aditya1134@gmail.com>
2025-10-18 00:14:34 +0530

    third commit

---
+++ projects/second_project/bmi.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form")
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const result = document.querySelector("#result")
+    if(height === '' || height < 0 || isNaN(height)){
+        result.innerHTML = "Please give a valid height"
+    }
+    else if(weight === '' || weight < 0 || isNaN(weight)){
+        result.innerHTML = "Please give a valid weight"
+    }
+    else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        result.innerHTML = `<span>${bmi}</span>`
+        const range = document.querySelector("#range")
+        if(bmi < 18.5){
+            range.innerHTML = "Underweight"
+        }
+        else if(bmi > 18.5 && bmi < 24.9){
+            range.innerHTML = "Normal"
+        }
+        else{
+            range.innerHTML = "Overweight"
+        }
+    }
+})
