062f7d39faa9abcc2fabc75d0fb662ffc64e0d6c
Aditya Raj <raj.aditya1134@gmail.com>
2025-10-17 18:17:09 +0530

    second commit

---
+++ projects/colorswitcher.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.body
+
+buttons.forEach(function (button) {
+    console.log(button)
+    button.addEventListener('click',function(e){
+        console.log(e)
+        console.log(e.target)
+        switch(e.target.id){
+            case "grey":
+                body.style.backgroundColor = e.target.id
+                break;
+            case "white":
+                body.style.backgroundColor = e.target.id
+                break;
+            case "blue":
+                body.style.backgroundColor = e.target.id
+                break;
+            case "yellow":
+                body.style.backgroundColor = e.target.id
+                break;
+            case "purple":
+                body.style.backgroundColor = e.target.id
+        }
+    })
+})
